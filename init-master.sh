900eed0612d56000a7688ecea371f1290f7cc6b6
c2pig <c2pig@c2pigs-iMac.local>
2021-10-22 15:25:41 +0800

    add control plane init script

--- init-master.sh
+++ init-master.sh
@@ -1,8 +1,8 @@
-sudo kubeadm init --pod-network-cidr 192.168.0.0/16 --kubernetes-version 1.22.0
+kubeadm init --pod-network-cidr 192.168.0.0/16 --kubernetes-version 1.22.0
 
 mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
+cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+chown $(id -u):$(id -g) $HOME/.kube/config
 
 kubectl get nodes
 
--- install-k8s-dependencies.sh
+++ install-k8s-dependencies.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-
 cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
 overlay
 br_netfilter
 EOF
 
-sudo modprobe overlay
-sudo modprobe br_netfilter
+modprobe overlay
+modprobe br_netfilter
 
 cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
 net.bridge.bridge-nf-call-iptables = 1
@@ -15,23 +14,23 @@ net.ipv4.ip_forward = 1
 net.bridge.bridge-nf-call-ip6tables = 1
 EOF
 
-sudo sysctl --system
+sysctl --system
 
-sudo apt-get update && sudo apt-get install -y containerd
+apt-get update && sudo apt-get install -y containerd
 
-sudo mkdir -p /etc/containerd
+mkdir -p /etc/containerd
 
-sudo containerd config default | sudo tee /etc/containerd/config.toml
+containerd config default | sudo tee /etc/containerd/config.toml
 
-sudo systemctl restart containerd
+systemctl restart containerd
 
-sleep 3 && sudo systemctl status containerd
+sleep 3 && systemctl status containerd
 
-sudo swapoff -a
+swapoff -a
 
-sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
 
-sudo apt-get update && sudo apt-get install -y apt-transport-https curl
+apt-get update && apt-get install -y apt-transport-https curl
 
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 
@@ -39,10 +38,10 @@ cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 
-sudo apt-get update
+apt-get update
 
-sudo apt-get install -y kubelet=1.22.0-00 kubeadm=1.22.0-00 kubectl=1.22.0-00
+apt-get install -y kubelet=1.22.0-00 kubeadm=1.22.0-00 kubectl=1.22.0-00
 
-sudo apt-mark hold kubelet kubeadm kubectl
+apt-mark hold kubelet kubeadm kubectl
 
 
